666f40a0e85c56c9b4acccf0aa0653a5ebe74171
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-07 09:06:22 -0700

    Taking notes during the intermediate youtube video

--- Intermediate-python-notes.py
+++ Intermediate-python-notes.py
@@ -4,3 +4,8 @@ print(myList)
 
 myList2 = list() #use list function to create and empty list
 print(myList2)
+
+# To create a second list that is in a different order use the step method
+myList2 = myList[::-1]
+print(myList2)
+
