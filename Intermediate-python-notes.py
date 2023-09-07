28832cadcdcba1778fa487dc0a01c7b2cbdba44d
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-07 08:24:14 -0700

    Beginning intermediate notes for python learning

---
+++ Intermediate-python-notes.py
@@ -0,0 +1,6 @@
+# Lists are created with square brackets
+myList = ["banna", "cherry", "apple", "orange"]
+print(myList)
+
+myList2 = list() #use list function to create and empty list
+print(myList2)
