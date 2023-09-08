c50d5b4e855658b6a1ff12c6a0bca0fa2765971c
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-08 09:38:49 -0700

    Create Python_Notes_Intermediate

--- Intermediate-python-notes.py
+++ Intermediate-python-notes.py
@@ -8,4 +8,7 @@ print(myList2)
 # To create a second list that is in a different order use the step method
 myList2 = myList[::-1]
 print(myList2)
+# Becareful of copying lists. If you copy list to list any mods with change original list. 
+# To copy use either .cop(), list(list_orig), or listcpy = list_Orig[:]
+
 
