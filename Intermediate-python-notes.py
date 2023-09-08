9518ab570c6e8bae840fb7b66e09da2d669f0025
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-08 13:41:59 -0700

    Updating notes

--- Intermediate-python-notes.py
+++ Intermediate-python-notes.py
@@ -11,4 +11,26 @@ print(myList2)
 # Becareful of copying lists. If you copy list to list any mods with change original list. 
 # To copy use either .cop(), list(list_orig), or listcpy = list_Orig[:]
 
+myList3 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+newList = [i*i for i in myList3]
+print(myList3)
+print(newList)
 
+#Tuples are a list created with () and once made cannot be changed.
+myTuples = ("clyde", 53, "Tracy")
+print(myTuples)
+# for tuples with only one item a comma must follow the item to make it a tuple
+myTuples2 = ("clyde",)
+print(type(myTuples2))
+
+myTuples3 = tuple(["clyde", 53, "Tracy"])# created using function tuple to use on a list
+print(myTuples3)
+
+#unpacking tuples:
+my_tuple1 = "Clyde", 53, "Tracy"
+name, age, city = my_tuple1
+
+print(name)
+print(age)
+print(city)
+print(type(my_tuple1))
