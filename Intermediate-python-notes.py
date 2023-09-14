0fe334aa7ea0ffe88a38c510d84b37aa10337338
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-13 17:45:25 -0700

    updating notes

--- Intermediate-python-notes.py
+++ Intermediate-python-notes.py
@@ -302,6 +302,21 @@ group_obj = groupby(persons, key=lambda x: x['age'])
 for key, value in group_obj:
     print(key, list(value))
 
+#Lambda function review: lambda arguments: expression
+add10 = lambda x: x + 10 
+print(add10(5)) # result is 15
+
+points2D = [(1, 2), (15, 1), (5, -1), (10, 4)]
+points2D_sorted = sorted(points2D, key=lambda x: x[1])
+
+print(points2D)
+print(points2D_sorted) # sorted by y index
+
+#map(func, seq(or a list))
+a=[1, 2, 3, 4, 5, 6]
+b = map(lambda x: x*2, a)
+print(list(b))
+
 
 
 
