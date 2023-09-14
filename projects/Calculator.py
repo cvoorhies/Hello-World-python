59e0bbf0f91bd59375a6761dff7b579e864e4b22
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-14 14:35:04 -0700

    updating how I created the Calculator

--- projects/Calculator.py
+++ projects/Calculator.py
@@ -1,24 +1,28 @@
 # functions needed
 # input variables
 # loop function to get inputs
+addSum = lambda x,y: x + y
+subSum = lambda x,y: x - y
+multiSum = lambda x,y: x * y
+divSum = lambda x,y: x / y
 a_input = 0.00
 b_input  = 0.00
 oper_sign = ''
 a_input = input('input first number: ')
-oper_sign = input("input oprator")
+oper_sign = input("input oprator: ")
 b_input = input('input second number: ')
 if oper_sign == '+':
-    a_input = float(a_input)+ float(b_input)
-    print (a_input)
+    result = addSum(float(a_input), float(b_input))
+    print (result)
 elif oper_sign == '-':
-    a_input = float(a_input)- float(b_input)
-    print (a_input)
+    result = subSum(float(a_input), float(b_input))
+    print (result)
 elif oper_sign == '*':
-    a_input = float(a_input)* float(b_input)
-    print (a_input)
+    result = multiSum(float(a_input), float(b_input))
+    print (result)
 elif oper_sign == '/':
     if b_input == '0':
         print("Cannot divide by 0")
     else:
-        a_input = float(a_input)/ float(b_input)
-        print (a_input)
+        result = divSum(float(a_input), float(b_input))
+        print (result)
