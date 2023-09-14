f8354af10c6f7b0ca541283cd10da8893c8ea384
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-14 11:09:40 -0700

    updating Calculator

--- projects/Calculator.py
+++ projects/Calculator.py
@@ -3,15 +3,13 @@
 # loop function to get inputs
 a_input = 0.00
 b_input  = 0.00
-oper_sign = '+'
+oper_sign = ''
 a_input = input('input first number: ')
 oper_sign = input("input oprator")
 b_input = input('input second number: ')
 if oper_sign == '+':
-    #a_input = float(a_input)+ float(b_input)
-    #print (a_input)
-    result = int(lambda a_input,b_input: a_input+b_input)
-    print(result)
+    a_input = float(a_input)+ float(b_input)
+    print (a_input)
 elif oper_sign == '-':
     a_input = float(a_input)- float(b_input)
     print (a_input)
