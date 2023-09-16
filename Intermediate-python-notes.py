b2f1147f05a017f9558840769b148024b09b881c
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-16 12:08:05 -0700

    updating notes

--- Intermediate-python-notes.py
+++ Intermediate-python-notes.py
@@ -379,6 +379,37 @@ print(product_a)
 
 # Logging 
 import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%y %H:%M:%S')
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message') # with just these 5 messages only the warning and higher will be printed if the basicConfig message isn't there
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
+import Helper
+######### handler logging
+logger = logging.getLogger(__name__)
+
+# Create handler
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler('file.log')
+
+# level and the format
+stream_h.setLevel(logging.WARNING)
+file_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logger.warning('this is a warning message')
+logger.error('this is an error message')
+
 import logging.config
 
 logging.config.fileConfig('logging.ini')
