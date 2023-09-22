2181c58dea8932bac979b66cc69cbb34bcfd4da6
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-21 18:55:12 -0700

    more JSON notes

--- JSON_Notes.py
+++ JSON_Notes.py
@@ -15,4 +15,23 @@ personJSON = json.dumps(person, indent=4, sort_keys=True)
 
 with open('person.json', 'r') as file: 
     person = json.load(file)
-    print(person)
+   # print(person)
+
+###***** from a class to a JSON file
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+user = User('Max', 27)
+
+def encode_user(o):
+    if isinstance(o, User):
+        return {'name': o.name, 'age': o.age, o.__class__.__name__ : True}
+    else:
+        raise TypeError('Object of type User is not JSON serializable')
+
+
+userJSON = json.dumps(user, default=encode_user)
+print(userJSON)
