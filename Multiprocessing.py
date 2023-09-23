02dc2aaa5c8fe0557fb7846ba173ea52afb0b0c3
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-23 16:52:55 -0700

    Adding threading notes

--- Multiprocessing.py
+++ Multiprocessing.py
@@ -3,29 +3,56 @@ import os
 import time
 
 def square_numbers():
-    for i in range(100):
-        i * i
+    for i in range(10):
+        print(i * i)
         time.sleep(0.1)
 
 
 processes = []
 num_processes = os.cpu_count()
 
+
 # Create processes
 for i in range(num_processes):
     p = Process(target=square_numbers)
     processes.append(p)
 
-# Start
-for p in processes:
-    p.start()
+if __name__ == '__main__':
+    # Start
+    for p in processes:
+        p.start()
 
-# join - means to wait for a process to finish and block the main thread
-for p in processes:
-    p.join()
+    # join - means to wait for a process to finish and block the main thread
+    for p in processes:
+        p.join()
 
-
+print(os.cpu_count())
 print('end main')
+#########
+from threading import Thread
+def square_numbers():
+    for i in range(10):
+        print(i * i)
+        time.sleep(0.1)
 
 
+threads = []
+num_threads = 10
 
+
+# Create processes
+for i in range(num_threads):
+    t = Thread(target=square_numbers)
+    threads.append(t)
+
+if __name__ == '__main__':
+    # Start
+    for t in threads:
+        t.start()
+
+    # join - means to wait for a process to finish and block the main thread
+    for t in threads:
+        t.join()
+
+#print(os.cpu_count())
+print('end main')
