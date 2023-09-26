bd721e8e6e6220cc87adaba618b1cdcc03f6f604
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-26 06:51:41 -0700

    update with pool notes

--- Multiprocessing.py
+++ Multiprocessing.py
@@ -1,23 +1,55 @@
-from multiprocessing import Process
-import os
+from multiprocessing import Process, Value, Array, Lock
 import time
 
-def square_numbers():
-    for i in range(10):
-        print(i * i)
+def add_100(numbers, lock):
+    for i in range(100):
         time.sleep(0.1)
+        for i in range(len(numbers)):
+            with lock:
+                numbers[i] += 1
+        
+if __name__ == '__main__':
+    lock =Lock()
+    shared_array = Array('d', [0.0, 100.0, 200.0])
+    #shared_number = Value('i', 0)
+    print('Array at beginning is', shared_array[:])#shared_number.value)
+
+    p1 = Process(target=add_100, args=(shared_array, lock))# shared_number, the args is a tuble so it needs the ',' to indicate to python that it is one.
+    p2 = Process(target=add_100, args=(shared_array, lock))
+    p3 = Process(target=add_100, args=(shared_array, lock))
+    p4 = Process(target=add_100, args=(shared_array, lock))
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join() #waits for process to stop.
+    p2.join()
+    p3.join() #waits for process to stop.
+    p4.join()
 
 
-processes = []
-num_processes = os.cpu_count()
+    print('Number at end is', shared_array[:])
+
+
+
+
+
+
+
+
+
+"""    processes = []
+    num_processes = os.cpu_count()
 
 
 # Create processes
-for i in range(num_processes):
-    p = Process(target=square_numbers)
-    processes.append(p)
+    for i in range(num_processes):
+        p = Process(target=square_numbers)
+        processes.append(p)
 
-if __name__ == '__main__':
+
     # Start
     for p in processes:
         p.start()
@@ -26,11 +58,11 @@ if __name__ == '__main__':
     for p in processes:
         p.join()
 
-print(os.cpu_count())
-print('end main')
+#print(os.cpu_count())
+print('end main')"""
 #########
 
-from threading import Thread
+"""from threading import Thread
 
 def square_numbers():
     for i in range(100):
@@ -58,3 +90,24 @@ if __name__ == '__main__':
 
     #print(os.cpu_count())
     print('end main')
+"""
+
+from multiprocessing import Pool
+import time
+
+def cube(number):
+    return number * number * number
+        
+if __name__ == '__main__':
+    
+    numbers = range(10)
+    pool = Pool()
+
+    #map, apply, join, close 
+    result = pool.map(cube, numbers)
+
+    pool.close()
+    pool.join()
+    
+
+    print(result)
