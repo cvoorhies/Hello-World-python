fd352a097a3772957804a6949cdd58e84dd5fa93
cvoorhies <MyOfficeSpace888@gmail.com>
2023-09-29 11:32:15 -0700

    Function notes

--- Function Arguments.py
+++ Function Arguments.py
@@ -1,1 +1,15 @@
+"""
+Difference between arguments and parameters
+   - Arguments are:
+   - Parameters are:
 
+Positional and keyword arguments
+
+Variable-length Arguments (*args and **kwargs)
+
+Container unpacking into a function arguments
+
+Local vs global arguments
+
+parameter passing (by value or by reference?)
+"""
