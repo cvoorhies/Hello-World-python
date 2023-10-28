364f0759e322fda6f7529e411a529b0a5fc4e5eb
cvoorhies <MyOfficeSpace888@gmail.com>
2023-10-28 16:04:23 -0700

    WOrking on building classes

--- Python_ATE/PyVISA/Automating_RFTests.py
+++ Python_ATE/PyVISA/Automating_RFTests.py
@@ -44,73 +44,132 @@ class ATE_test:
 
     def exceptionHandler(exception):
 
-    print('Error information:\n\tAbbreviation: %s\n\tError code: %s\n\tDescription: %s' % \
-          (exception.abbreviation, exception.error_code, exception.description))
+        print('Error information:\n\tAbbreviation: %s\n\tError code: %s\n\tDescription: %s' % \
+            (exception.abbreviation, exception.error_code, exception.description))
 
 
-# Change this variable to the address of your instrument
-VISA_ADDRESS = 'Your instruments VISA address goes here!'
+    # Change this variable to the address of your instrument
+    VISA_ADDRESS = 'Your instruments VISA address goes here!'
 
-# Part 1:
-#
-# Shows the mechanics of how to deal with an error in PyVISA when it occurs.
-# To stimulate an error, the code will try to open a connection to an instrument at an invalid address...
-#
-# First we'll provide an invalid address and see what error we get
-
-resourceManager = visa.ResourceManager()
-
-try:
-    session = resourceManager.open_resource("BAD ADDRESS")
-except visa.VisaIOError as ex:
-    print('VISA ERROR - An error has occurred!\n')
-
-    # To get more specific information about the exception, we can check what kind of error it is and
-    # add specific error handling code. In this example, that is done in the exceptionHandler function
-    exceptionHandler(ex)
-
-    # Part 2:
+    # Part 1:
     #
-    # Stimulate another error by sending an invalid query and trying to read its response.
+    # Shows the mechanics of how to deal with an error in PyVISA when it occurs.
+    # To stimulate an error, the code will try to open a connection to an instrument at an invalid address...
     #
-    # Before running this part, don't forget to set your instrument address in the 'VISA_ADDRESS'
-    # variable at the top of this script
+    # First we'll provide an invalid address and see what error we get
 
-    session = resourceManager.open_resource(VISA_ADDRESS)
+    resourceManager = visa.ResourceManager()
 
-    # Misspell the *IDN? query as *IND?
     try:
-        session.write('*IND?')
-    except visa.VisaIOError as ex2:
-        print(
-            'VISA ERROR - You\'ll never get here, because the *IND? data will get sent to the instrument successfully, '
-            'it\'s the instrument that won\'t like it.')
+        session = resourceManager.open_resource("BAD ADDRESS")
+    except visa.VisaIOError as ex:
+        print('VISA ERROR - An error has occurred!\n')
 
-    # Try to read the response (*IND ?)
-    try:
-        idnResponse = session.read()
-        print('*IDN? returned: %s\n' % idnResponse)
-    except visa.VisaIOError as ex3:
-        print('VISA ERROR - The read call will timeout, because the instrument doesn\'t'
-            ' know what to do with the command that we sent it.')
+        # To get more specific information about the exception, we can check what kind of error it is and
+        # add specific error handling code. In this example, that is done in the exceptionHandler function
+        exceptionHandler(ex)
 
-    # Check the instrument to see if it has any errors in its queue
-    rawError = ''
-    errorCode = -1
+        # Part 2:
+        #
+        # Stimulate another error by sending an invalid query and trying to read its response.
+        #
+        # Before running this part, don't forget to set your instrument address in the 'VISA_ADDRESS'
+        # variable at the top of this script
 
-    while errorCode != 0:
-        session.write('SYST:ERR?')
-        rawError = session.read()
+        session = resourceManager.open_resource(VISA_ADDRESS)
 
-        errorParts = rawError.split(',')
-        errorCode = int(errorParts[0])
-        errorMessage = errorParts[1].rstrip('\n')
+        # Misspell the *IDN? query as *IND?
+        try:
+            session.write('*IND?')
+        except visa.VisaIOError as ex2:
+            print(
+                'VISA ERROR - You\'ll never get here, because the *IND? data will get sent to the instrument successfully, '
+                'it\'s the instrument that won\'t like it.')
 
-        print('INSTRUMENT ERROR - Error code: %d, error message: %s' % (errorCode, errorMessage))
+        # Try to read the response (*IND ?)
+        try:
+            idnResponse = session.read()
+            print('*IDN? returned: %s\n' % idnResponse)
+        except visa.VisaIOError as ex3:
+            print('VISA ERROR - The read call will timeout, because the instrument doesn\'t'
+                ' know what to do with the command that we sent it.')
 
-    # Close the connection to the instrument
-    session.close()
-    resourceManager.close()
+        # Check the instrument to see if it has any errors in its queue
+        rawError = ''
+        errorCode = -1
+
+        while errorCode != 0:
+            session.write('SYST:ERR?')
+            rawError = session.read()
+
+            errorParts = rawError.split(',')
+            errorCode = int(errorParts[0])
+            errorMessage = errorParts[1].rstrip('\n')
+
+            print('INSTRUMENT ERROR - Error code: %d, error message: %s' % (errorCode, errorMessage))
+
+        # Close the connection to the instrument
+        session.close()
+        resourceManager.close()
+
+        print('Done.')
+
+        ################################################################################
+    # © Keysight Technologies 2016
+    #
+    # You have a royalty-free right to use, modify, reproduce and distribute
+    # the Sample Application Files (and/or any modified version) in any way
+    # you find useful, provided that you agree that Keysight Technologies has no
+    # warranty, obligations or liability for any Sample Application Files.
+    #
+    ################################################################################
+
+    import visa
+
+
+    def find(searchString):
+
+        resourceManager = visa.ResourceManager()
+
+        print('Find with search string \'%s\':' % searchString)
+        devices = resourceManager.list_resources(searchString)
+        if len(devices) > 0:
+            for device in devices:
+                print('\t%s' % device)
+        else:
+            print('... didn\'t find anything!')
+
+        resourceManager.close()
+
+
+    # Finding all devices and interfaces is straightforward
+    print('Find all devices and interfaces:\n')
+    find('?*')
+
+    # You can specify other device types using different search strings. Here are some common examples:
+
+    # All instruments (no INTFC, BACKPLANE or MEMACC)
+    find('?*INSTR')
+    # PXI modules
+    find('PXI?*INSTR')
+    # USB devices
+    find('USB?*INSTR')
+    # GPIB instruments
+    find('GPIB?*')
+    # GPIB interfaces
+    find('GPIB?*INTFC')
+    # GPIB instruments on the GPIB0 interface
+    find('GPIB0?*INSTR')
+    # LAN instruments
+    find('TCPIP?*')
+    # SOCKET (::SOCKET) instruments
+    find('TCPIP?*SOCKET')
+    # VXI-11 (inst) instruments
+    find('TCPIP?*inst?*INSTR')
+    # HiSLIP (hislip) instruments
+    find('TCPIP?*hislip?*INSTR')
+    # RS-232 instruments
+    find('ASRL?*INSTR')
 
     print('Done.')
 
