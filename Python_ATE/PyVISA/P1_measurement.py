e4ca2385e2f0200fac6f31aae29301bcb2b37ac2
cvoorhies <MyOfficeSpace888@gmail.com>
2023-11-07 07:01:36 -0800

    Save files

--- Python_ATE/PyVISA/P1_measurement.py
+++ Python_ATE/PyVISA/P1_measurement.py
@@ -20,65 +20,73 @@ from time import sleep
 # Example for ethernet pwr_supply = rm.open_resource('TCPIP::192.168.128.24::INSTR')
 # to get a list of stuff on the net use rm.list_resources()
 
-# Connect to the power meter and signal generator
-pm = Pm('GPIB0::16::INSTR') #pyvisa.ResourceManager().open_resource('GPIB0::16::INSTR')
-sg = SigGen('GPIB0::16::INSTR') #pyvisa.ResourceManager().open_resource('GPIB0::16::INSTR')
-#while True: # Need a condition to run this in ?? steps
-print(Quantity(mp_pm.query('MEAS?').strip(), "Ohms"))
-sleep(1)
-# Get the power measurement
-power = pm.query('MEAS:POW?')
+def P1meas(cntrF):
+    # Connect to the power meter and signal generator
+    pm = pyvisa.ResourceManager('GPIB0::16::INSTR') #pyvisa.ResourceManager().open_resource('GPIB0::16::INSTR')
+    sg = pyvisa.ResourceManager('GPIB0::18::INSTR') #pyvisa.ResourceManager().open_resource('GPIB0::16::INSTR')
+    #while True: # Need a condition to run this in ?? steps
+    print(Quantity(mp_pm.query('MEAS?').strip(), "Ohms"))
+    sleep(1)
+    # Get the power measurement
+    power = pm.query('MEAS:POW?')
 
-# Set the frequency to 1 GHz
-sg.write('SOUR:FREQ 1000-000-000')
+    freqset = f'SOUR:FREQ {cntrF}'
+    # Set the frequency to 1 GHz
+    sg.write(freqset)
 
-# Set the power to 10 dBm
-sg.write('SOUR:POW 10')
+    # Set the power to 10 dBm
+    sg.write('SOUR:POW 10')
 
-# Turn on the output
-sg.write('OUTP ON')
+    # Turn on the output
+    sg.write('OUTP ON')
 
-# Generate a signal
-sg.write('INIT:IMM')
+    # Generate a signal
+    sg.write('INIT:IMM')
 
-# Get the power measurement
-power = pm.query('MEAS:POW?')
+    # Get the power measurement
+    power = pm.query('MEAS:POW?')
 
-# Set the power to -10 dBm
-sg.write('SOUR:POW -10')
-sleep(1)
+    # Set the power to -10 dBm
+    sg.write('SOUR:POW -10')
+    sleep(1)
 
-powerdelta = 10
-lastPwrdg = 0
-pwrSetVar = 0
-while(powerdelta <=9):
-    lastPwrdg = pm.query('MEAS:POW?') # expect reading to start at -10dbm, then increase by 10.
-    
-    pwset = f'SOUR:POW {pwrSetVar}'
+    powerdelta = 10
+    lastPwrdg = 0
+    pwrSetVar = 0
+    while(powerdelta <=9):
+        lastPwrdg = pm.query('MEAS:POW?') # expect reading to start at -10dbm, then increase by 10.
+        
+        pwset = f'SOUR:POW {pwrSetVar}'
+        sg.write(pwset)
+        sleep(1)
+        power = pm.query('MEAS:POW?') # expect reading to be 0dbm on first round. increment by 10 after.
+        powerdelta = power - lastPwrdg
+        pwrSetVar += 10
+
+    power -= 10  # Set to a 10 db backoff
+    powerdelta = 10
+    pwset = f'SOUR:POW {power}'
     sg.write(pwset)
     sleep(1)
-    power = pm.query('MEAS:POW?') # expect reading to be 0dbm on first round. increment by 10 after.
-    powerdelta = power - lastPwrdg
-    pwrSetVar += 10
+    # repeat after 10 db backoff and increment by 1
+    while(powerdelta <=9):
+        lastPwrdg = pm.query('MEAS:POW?') # expect reading to start at -10dbm, then increase by 10.
+        
+        pwset = f'SOUR:POW {pwrSetVar}'
+        sg.write(pwset)
+        sleep(1)
+        power = pm.query('MEAS:POW?') # expect reading to be 0dbm on first round. increment by 10 after.
+        powerdelta = power - lastPwrdg
+        pwrSetVar += 1
+        
+        sg.write('SOUR:POW 1') # set back to p1 then measure
+        power = pm.query('MEAS:POW?')
+        return power
+    # Close the connection to the Signal Generator
+    sg.close()
 
-power -= 10  # Set to a 10 db backoff
-powerdelta = 10
-pwset = f'SOUR:POW {power}'
-sg.write(pwset)
-sleep(1)
-# repeat after 10 db backoff and increment by 1
-while(powerdelta <=9):
-    lastPwrdg = pm.query('MEAS:POW?') # expect reading to start at -10dbm, then increase by 10.
-    
-    pwset = f'SOUR:POW {pwrSetVar}'
-    sg.write(pwset)
-    sleep(1)
-    power = pm.query('MEAS:POW?') # expect reading to be 0dbm on first round. increment by 10 after.
-    powerdelta = power - lastPwrdg
-    pwrSetVar += 1
+        # Close the connection to the Power Meter
+    pm.close()
 
-# Close the connection to the Signal Generator
-sg.close()
-
-# Close the connection to the Power Meter
-pm.close()
+result = P1meas(8000000000)
+print(result)
