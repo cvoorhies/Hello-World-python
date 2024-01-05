b1ded9622f8409e2f98c4512d79841cc7ff197da
cvoorhies <MyOfficeSpace888@gmail.com>
2024-01-05 07:00:52 -0800

    added a password generator to my games

---
+++ Password Generator/PWG.py
@@ -0,0 +1,25 @@
+import random
+
+print('Welcome to you Password Generator')
+
+chars = 'abcdefghijklmnopqurtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()1234567890'
+
+num_PW = input('Amount of passwords to generate: ')
+num_PW = int(num_PW)
+
+len_PW = input('Input the length of your password: ')
+len_PW = int(len_PW)
+
+print('\nHere are your passwords:')
+
+for pwd in range(num_PW):
+    password = ''
+    for c in range(len_PW):
+        password += random.choice(chars)
+
+    print(password)
+
+input('don\'t forget to write down your passwords in a safe place')
+          
+
+
