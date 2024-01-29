ee265fd2693839385843fc3f33504bb598bc906f
cvoorhies <MyOfficeSpace888@gmail.com>
2024-01-28 17:46:03 -0800

    finished List Comp task

--- TestBed.py
+++ TestBed.py
@@ -1,6 +1,6 @@
-import itertools
+"""import itertools
 
-"""n=5
+n=5
 mystring = []
 for i in range(n):
     mystring.append(i+1)
@@ -8,25 +8,23 @@ for i in range(n):
     i += i
     
 newstring = ''.join(str(e) for e in mystring)
-print(newstring)"""
+print(newstring)
 
+#list comprehensions!
 x = 1
 y = 1
 z = 2
 n = 3
+#i=0
+def find_valid_coordinates(x, y, z, n):
+    valid_coords = []
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i + j + k != n:
+                    valid_coords.append([i,j,k])
+    return valid_coords
 
-mystring1 = [x, y, z]
-#mystring2 = [0, 0]
-#mystring3 = [0, 0]
-#0<=i<=x, 0<=j<=y, 0<=k<=z
+origList = find_valid_coordinates(x, y, z, n)
+print (origList)"""
 
-print([[0<=x<=x, 0<=y<=y, 0<=z<=z] for x in range(1,n+1)])
-
-#print (mystring1)
-
-
-origList = [[0,0,0],[0,0,1],[0,0,2],[0,1,0],[0,1,1],[0,1, 2],[1, 0,0],[1,0,1],[1,0,2],[1,1,0],[1,1,1],[1,1,2]]
-i=0
-myCombo = list(itertools.permutations(mystring1, n))
-myCombo = list(itertools.combinations(myCombo, n))
-#print(myCombo)
